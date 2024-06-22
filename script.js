9769f258cdd8e5cb60ab7f10660b7daa0f281bd1
akin <akin.isletme@gmail.com>
2024-06-22 16:36:21 +0200

    Added getComputerChoice function to the script
    Created the first random function of the game

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!")
+let x = Math.floor(Math.random() * 100);
+
+let choice = "";
+
+function getComputerChoice(x, choice) {
+    if (x >= 0 && x <= 33) {
+        return console.log(choice = "rock");
+    } else if (x >= 34 && x <= 66) {
+        return console.log(choice = "paper");
+    } else if (x >= 67 && x <= 99) {
+        return console.log(choice = "scissors");
+    }
+}
+
+getComputerChoice(x, choice);
