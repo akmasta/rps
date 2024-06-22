bb69d8589cee2a9dc08b89be118d92ec73037dd5
akin <akin.isletme@gmail.com>
2024-06-22 21:03:40 +0200

    added gameplay loop
    Finalize game and make it actually playable

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let x = Math.floor(Math.random() * 100);
-let compChoice = "";
-let humChoice = prompt("What's your choice? (1) rock / (2) paper / (3) scissors");
-humanScore = 0;
-compScore = 0;
-
 // Make computer choose randomly.
 
-function getComputerChoice(x, compChoice) {
-    if (x >= 0 && x <= 33) {
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() * 100);
+    if (compChoice >= 0 && compChoice <= 33) {
         return compChoice = "rock";
-    } else if (x >= 34 && x <= 66) {
+    } else if (compChoice >= 34 && compChoice <= 66) {
         return compChoice = "paper";
-    } else if (x >= 67 && x <= 99) {
+    } else {
         return compChoice = "scissors";
     }
 }
 
-getComputerChoice(x, compChoice);
-
-
 // Prompt for user choice.
 
-function getHumanChoice(humChoice) {
+function getHumanChoice() {
+    let humChoice = prompt("What's your choice? (1) rock / (2) paper / (3) scissors");
     if (humChoice === "1") {
         return humChoice = "rock";
     } else if (humChoice === "2") {
@@ -33,27 +26,51 @@ function getHumanChoice(humChoice) {
     }
 }
 
-getHumanChoice(humChoice);
+// Gameplay - individual round
 
-// Gameplay
+let humanScore = 0;
+let compScore = 0;
 
 function playRound(humChoice, compChoice) {
     if (humChoice === "rock" && compChoice === "scissors"
         || humChoice === "paper" && compChoice === "rock"
         || humChoice === "scissors" && compChoice === "paper") {
-        return console.log(`${humChoice} vs. ${compChoice} - you win!`);
+        return [console.log(humanScore++, `${humChoice} vs. ${compChoice} - you win! human: ${humanScore} - computer: ${compScore}`)];
     } else if (humChoice === "rock" && compChoice === "paper"
         || humChoice === "paper" && compChoice === "scissors"
         || humChoice === "scissors" && compChoice === "rock") { 
-        return console.log(`${humChoice} vs. ${compChoice} - computer wins!`);
+        return console.log(compScore++, `${humChoice} vs. ${compChoice} - computer wins! human: ${humanScore} - computer: ${compScore}`);
     } else if (humChoice === "rock" && compChoice === "rock" 
         || humChoice === "paper" && compChoice === "paper"
         || humChoice === "scissors" && compChoice === "scissors") {
-        return console.log(`${humChoice} vs. ${compChoice} - it's a tie!`);
+        return console.log(`${humChoice} vs. ${compChoice} - it's a tie! human: ${humanScore} - computer: ${compScore}`);
+    } else {
+        return console.log("Try again.");
     }
 }
 
-const humSelection = getHumanChoice(humChoice);
-const compSelection = getComputerChoice(x, compChoice);
+// Gameplay - full game
 
-playRound(humSelection, compSelection);
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humSelection = getHumanChoice();
+        if (humSelection === "Invalid!") {
+            console.log("Invalid. Please choose (1) rock, (2) paper, or (3) scissors.");
+            i--; // Repeat if input is invalid
+            continue;
+        }
+        const compSelection = getComputerChoice();
+        playRound(humSelection, compSelection);
+    }
+    
+    // Display winner
+    if (humanScore > compScore) {
+        console.log("you win the game!");
+    } else if (compScore > humanScore) {
+        console.log("computer wins the game!");
+    } else {
+        console.log("tie!");
+    }
+}
+
+playGame ();
